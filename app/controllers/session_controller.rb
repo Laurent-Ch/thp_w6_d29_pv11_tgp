2f208001bae55bdfb7690c56a518061b79f7c0cd
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-11-05 16:30:24 +0100

    friday's work added

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,8 @@ class SessionController < ApplicationController
     user = User.find_by(email: params[:user_email])
 
     if user && user.authenticate(params[:user_password])
-      session[:user_id] = user.id
+      log_in(user)
+      remember(user)
       redirect_to root_path, success: 'Bienvenue !'
 
     else
@@ -17,7 +18,9 @@ class SessionController < ApplicationController
   end
 
   def destroy
-    session.delete(:user_id)
+    # session.delete(:user_id)
+    user = current_user
+    log_out(user)
     redirect_to root_path
   end
 end
